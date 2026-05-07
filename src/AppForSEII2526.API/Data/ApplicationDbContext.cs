dd941ecae9273c1ee410ceda4acd78852e4a7b7e
VelaElDios <167448563+VelaElDios@users.noreply.github.com>
2026-05-07 23:30:56 +0200

    Finalizado desarrollo de clases y base de datos para CU1 Comprar Dispositivo (#2)
    Co-authored-by: Vela <Vela@VELAELDIOS>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -2,7 +2,20 @@
 using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.API.Models;
 
-namespace AppForSEII2526.API.Data;
+namespace AppForSEII2526.API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+        // --- PEGA TUS CLASES AQUÍ ---
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<PurchaseItem> PurchaseItems { get; set; }
+        // ----------------------------
+    }
 }
---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace AppForSEII2526.API.Models
+{
+    public enum PaymentMethod
+    {
+        CreditCard,
+        PayPal
+    }
+
+    public class Purchase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Nombre del Cliente")]
+        public string CustomerUserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Apellidos del Cliente")]
+        public string CustomerUserSurname { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Dirección de Entrega")]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Método de Pago")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [Display(Name = "Fecha de Compra")]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Precio Total")]
+        public double TotalPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Cantidad Total")]
+        public int TotalQuantity { get; set; }
+
+        public virtual IList<PurchaseItem> PurchaseItems { get; set; }
+    }
+}
